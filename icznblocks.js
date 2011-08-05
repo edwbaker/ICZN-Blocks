08362b1e78b9717da0afd2c799469d02158779ca
Ed Baker <edwbaker+github@gmail.com>
2011-08-05 12:33:04 +0100

    Initial commit

---
+++ icznblocks.js
@@ -0,0 +1,72 @@
+  function the_iczn() {
+    $("#iczn_front").html('<p><a href="http://iczn.org/files/images/Haeckel_Discomedusae_8.preview.jpg"><img src="http://www.iczn.org/sites/iczn.org/files/iczn_logo.gif" style="float: right;" height="150"></a><p style="text-align: left;">The International Commission on Zoological Nomenclature (ICZN) acts as adviser and arbiter for the zoological community by generating and disseminating information on the correct use of the scientific names of animals.<br><br>The ICZN is responsible for producing the International Code of Zoological Nomenclature - a set of rules for the naming of animals and the resolution of nomenclatural problems</p><p style="font-size: 11px;">Image: <em>Gryllotalpa gryllotalpa</em> (Linneaus, 1758). Complete version of image used on the cover of the Bulletin of Zoological Nomenclature. <a href="content/mole-cricket">More details.</a></p>');
+    $('#tab_iczn').css('background-color', 'rgb(6, 52, 85)');
+  }
+  function the_code () {
+    $("#iczn_front").html('<p><img src="http://iczn.org/sites/iczn.org/files/imagecache/preview/images/code.jpg" style="float:right;" height="150px" />The ICZN has responsibility for producing the International Code of Zoological Nomenclature - a set of rules for the naming of animals and the resolution of nomenclatural problems.</p><p>Copies of the code may be obtained from the International Trust for Zoological Nomenclature or the American Association for Zoological Nomenclature (<a href="http://iczn.org/content/order-copy-international-code-zoological-nomenclature-0">how to order</a>).</p><p>An <a href="http://www.nhm.ac.uk/hosted-sites/iczn/code/">online version</a> of the Code is freely available, as are translations in <a href="http://iczn.org/other_languages">other languages</a>.</p>');
+    $('#tab_code').css('background-color', 'rgb(6, 52, 85)');
+  }
+  function the_bulletin () {
+    $("#iczn_front").html('<p><a http://iczn.org/category/browse-bulletin/browse"><img src="http://iczn.org/sites/iczn.org/files/imagecache/thumbnail/images/BZN-68-2011-Cover.jpg" style="float: right;" height="150"></a>The Bulletin of Zoological Nomenclature publishes Cases made to the Commission, Comments on Cases by the zoological community and Opinions (rulings made by the Commission).</p><p>The <a href="http://iczn.org/category/browse-bulletin/browse">online version</a> of the BZN contains the abstracts of Cases and Opinions, and the full text of Comments.</p><p><a href="http://iczn.org/content/order-bulletin">How to subscribe to the print version of BZN</a>.</p>');
+    $('#tab_bulletin').css('background-color', 'rgb(6, 52, 85)');
+  }
+  function official_lists () {
+    $("#iczn_front").html('<p><a href="http://iczn.org/content/official-lists-indexes-1"><img src="http://iczn.org/sites/iczn.org/files/images/Official_Lists.jpg" style="float: right;" height="150"></a>The Official Lists and Indexes of Names and Works in Zoology (published in 1987 in a single volume - ISBN 0 85301 004 8) gave details of all the names and works on which ICZN had ruled since it was set up until 1985; more than 9900 names were listed.</p><p>In the 15 years between 1986 and the end of 2000 a further 601 Opinions and Directions have been published in the Bulletin listing 2,371 names and 14 works placed on the Official Lists and Indexes. Details of these 2,385 entries are given in a Supplement of 141 pages (ISBN 0 85301 007 2) published in March 2001.</p><p><a href="http://iczn.org/content/official-lists-indexes-1">Order a copy</a>.');
+    $('#tab_lists').css('background-color', 'rgb(6, 52, 85)');  
+  }
+  function donate () {
+    $("#iczn_front").html('<p>Read more about <a href="http://iczn.org/content/what_we_do">what we do and the importance of the ICZN</a>.</p><p align="center"><div style="margin-left:auto;margin-right:auto;padding-top:5px;" align="center"><form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input name="cmd" type="hidden" value="_s-xclick"><input name="encrypted" type="hidden" value="-----BEGIN PKCS7-----MIIHbwYJKoZIhvcNAQcEoIIHYDCCB1wCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCaIyHQmV0Xy9M2ACgjkFwJoJsWqP1TT1ZGSQTAdyXxS7iMjQYXdZ4Oy6zdQdCgYm7v5jZcaITj55oxV7OeRpu0a0zNgxZYBUpJCFmZr+zARhOAME+CqsiPgygwYO9A1uYe/W+2sxdBXAnsPRr9swNwxhpXjOsuTgHpZuar4Cm67jELMAkGBSsOAwIaBQAwgewGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQILShfPxnavhSAgchV2wp3mDfOI7wWPvyxT5a6yVLPYE/Yqs2mw75xt2pxFXbKd7iI88J9Jo+JSOrW40ffEYEmR1vWvakapXY28XuiUrtc/6apuVuFh7i4Gexw6QSj7AKPgPLQepERJR77SVOAG9syQ6gm9BPA01sRNRRA9KOPUIT0EBQ7/HPiMAJ+SQUoFbYJST7XwLJwE4MhRjTFQxPmrkXoGnewuPYfnSmTjPjp2bfnmc2WHNKBNAYwt6s5gG0IxSEmkf+fwmC8D4y7vBJCZZtEMKCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTEwMDkxNjE0MTcxN1owIwYJKoZIhvcNAQkEMRYEFFkNi08GMZ+fQz+Yk1XSq97lIsBNMA0GCSqGSIb3DQEBAQUABIGAFDVqzEUb8M+UehcLav0nEBb5+VxpMRDzk9IAVURV0PgzrG1IticViXejZZD0kmYBl6278ElNyztrAcF4DYfINd/ifoqtPmwWj54dZWsvUhWT0ESf+y3kS/HbnwHdys1aw16HNaA9y5c5NhApzx8Micj0PBnjP7cnxKwwYCfoR8w=-----END PKCS7-----"><input alt="PayPal - The safer, easier way to pay online." name="submit" src="https://www.paypal.com/en_US/GB/i/btn/btn_donateCC_LG.gif" type="image"><img src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1" border="0" style="display: block; margin-left: auto; margin-right: auto;"></form></div></p><p>You may also make a donation by using our <a href="http://iczn.org/content/make-donation">donation forms</a></p>.');
+    $('#tab_donate').css('background-color', 'rgb(6, 52, 85)');  
+  }
+  
+  
+  function iczn_tabs_none() {
+    $('.tabs_iczn').css('background-color', 'gray');
+  }
+
+  function iczn_panel_change(page) {
+    iczn_tabs_none();
+    switch(page) {
+      case 'the_iczn':
+        $('#iczn_front').eq(0).fadeOut('def', function () {
+          the_iczn();
+        });
+        break;
+      case 'the_code':
+        $('#iczn_front').eq(0).fadeOut('def', function () {
+          the_code();
+        });
+        break;
+      case 'the_bulletin':
+        $('#iczn_front').eq(0).fadeOut('def', function () {
+          the_bulletin();
+        });
+        break;
+      case 'official_lists':
+        $('#iczn_front').eq(0).fadeOut('def', function () {
+          official_lists();
+        });
+        break;
+	  case 'donate':
+	    $('#iczn_front').eq(0).fadeOut('def', function () {
+		  donate();
+		});
+		break;
+		
+	  default:
+	    alert('At least it gets here!');
+		break;
+    }
+    $('#iczn_front').eq(0).fadeIn('def');
+  }
+
+
+
+  function iczn_init() {
+    iczn_tabs_none();
+    the_iczn();
+  }
+  
+  Drupal.behaviors.icznblocksload = function () {
+    iczn_init();
+  };
